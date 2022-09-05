caa58ee21b68492c73ce77ece77c884dcafb7827
Kelly Duran <kaduran16@gmail.com>
2022-09-05 18:34:35 -0400

    commit tree e61cda06beb561c10229bbafa942b695f2801dac

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,19 @@
-# your code here
+class Stack
+    attr_accessor :stack
+
+    def initialize
+        @stack = []
+    end
+
+    def push(value)
+       @stack.push(value) 
+    end
+
+    def pop 
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+end
